3a14dfdcfbcd3d49a56c2c141a5b7cd7f9206c0a
glaucogithub <75082173+glaucogithub@users.noreply.github.com>
2021-06-30 15:04:08 -0300

    Add files via upload

---
+++ Iniciante/Lista de Exercicios/Interacao/Crescimento Populacional/sol.cpp
@@ -0,0 +1,54 @@
+
+// author: glaucoacassioc
+// created on: June 29, 2021 9:58 PM
+
+// Problem: Crescimento Populacional
+// URL: https://www.urionlinejudge.com.br/judge/pt/problems/view/1160
+// Time Limit: 1000 ms
+// Memory Limit: 1024 MB
+
+#include <bits/stdc++.h>
+using namespace std;
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template < typename T_container, typename T = typename enable_if < !is_same<T_container, string>::value, typename T_container::value_type >::type > ostream & operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifndef ACASSIO_DEBUG
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+#endif
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		int64_t pa, pb;
+		double g1, g2;
+		cin >> pa >> pb >> g1 >> g2;
+		int years = 0;
+		while (pa <= pb) {
+			int64_t p1 = (pa * g1) / 100, p2 = (pb * g2) / 100;
+			pa += p1, pb += p2;
+			// dbg(pa, pb);
+			years++;
+			if (years > 100) {
+				break;
+			}
+		}
+		if (years > 100) {
+			cout << "Mais de 1 seculo." << '\n';
+		} else {
+			cout << years << " anos." << '\n';
+		}
+	}
+}
